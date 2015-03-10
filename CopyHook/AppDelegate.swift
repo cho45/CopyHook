31fb5fe35db3fb9e46141449aedd1e81763efad4
cho45 <cho45@lowreal.net>
2015-03-10 21:36:23 +0900

    Initial Commit

---
+++ CopyHook/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CopyHook
+//
+//  Created by Satoh on 2015/03/10.
+//  Copyright (c) 2015年 Satoh. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
